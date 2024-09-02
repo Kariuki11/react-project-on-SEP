6e3e79f892dfaf3a59371ac393a2086bc9e942ca
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 16:03:25 +0300

    New Changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -19,7 +19,7 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
