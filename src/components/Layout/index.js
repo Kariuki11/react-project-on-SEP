fed4f0f2fc1da4fb0721c3a883f7eb51261192e3
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 17:21:13 +0300

    New changes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,24 +1,73 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () => {
     return (
-    <div className="App">
-        <Sidebar />
-        <div className='page'>
-            <span className='tags top-tags'>&alt;body&gt;</span>
+        <div className="App">
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
 
-            <Outlet />
+                <Outlet />
 
-            <span className='tags bottom-tags'>
-                &alt;/body&gt;
-                <br />
-                <span className='bottom-tag-html'>&lt;/html&gt;</span>
-            </span>
+                <span className='tags bottom-tags'>
+                    &lt;/body&gt;
+                    <br />
+                    <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
 
 export default Layout;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Outlet } from 'react-router-dom';
+// import Sidebar from '../Sidebar'
+// import './index.scss';
+
+// const Layout = () => {
+//     return (
+//     <div className="App">
+//         <Sidebar />
+//         <div className='page'>
+//             <span className='tags top-tags'>&alt;body&gt;</span>
+
+//             <Outlet />
+
+//             <span className='tags bottom-tags'>
+//                 &alt;/body&gt;
+//                 <br />
+//                 <span className='bottom-tag-html'>&lt;/html&gt;</span>
+//             </span>
+//         </div>
+//     </div>
+//     )
+// }
+
+// export default Layout;
