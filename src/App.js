5f6b8177c7004312ec886bae9d3b101fb70c9109
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 15:12:37 +0300

    New changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.scss';
-import Routes from 'react-router-dom';
+import { Routes, Route }from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
