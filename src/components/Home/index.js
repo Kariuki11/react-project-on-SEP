432b1763ce0255bbe5b8983700c38fa97c09f935
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 22:16:09 +0300

    New changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,13 +12,20 @@ const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br /> 
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer"/>
                 <AnimatedLetters letterClass={letterClass}
                 strArray={nameArray}
                 idx={15}/>
                 <br />
-                Web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}/>
                 </h1>
                 <h2>Software Developer / Python Expert / Student</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
