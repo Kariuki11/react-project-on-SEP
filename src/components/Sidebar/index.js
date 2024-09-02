a1584529d3489a809ccfe7f6edeea20c1ca30920
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 16:15:21 +0300

    New changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,7 +13,7 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" to="/"> 
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
