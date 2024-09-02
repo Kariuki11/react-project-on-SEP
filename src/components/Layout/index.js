85d34012d0a0ce7733afff96c84e1f17d4f14b51
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 15:19:10 +0300

    Created the Sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,10 @@
+import Sidebar from '../Sidebar'
 import './index.scss';
 
 const Layout = () => {
-    return <>Hello</>
+    return <>
+    <Sidebar />
+    </>
 }
 
 export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+
+const Sidebar = () => <>Hello from Sidebar</>
+
+export default Sidebar;
