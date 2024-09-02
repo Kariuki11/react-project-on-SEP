5097028b46a317f8572738bfc94b1d668e4dc0ad
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 23:09:14 +0300

    New changes

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,3 +1,20 @@
-import './index.scss'
 
-const Logo = () => ('Hello')
+
+
+
+
+
+
+
+// import './index.scss'
+// import LogoS from '../../../assets/images/logo-s (1).png'
+
+// const Logo = () => {
+//     return (
+//         <div className='logo-container'>
+//             <img className='solid-logo' src={LogoS} alt="K" />
+//         </div>
+//     )
+// }
+
+// export default Logo;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,10 +3,17 @@ import LogoTitle from '../../assets/images/logo-s (1).png'
 import './index.scss';
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['E', 'N', 'N']
-    const jobArray = ['W',
+    const nameArray = [
+        'E',
+        'N',
+        'N'
+    ]
+
+    const jobArray = [
+        'W',
         'e',
         'b',
         ' ',
@@ -26,27 +33,6 @@ const Home = () => {
             setLetterClass('text-animate-hover');
         }, 4000);
     }, []);
-    
-
-    // useEffect(() => {
-    //     return setTimeout(() => {
-    //         setLetterClass('text-animate-hover');
-    //     }, 4000);
-    // }, []);
-        
-        
-    //     (() => {
-    //         setLetterClass('text-animate-hover');
-    //     }, 4000)
-    // }, [])
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setLetterClass('text-animate-hover');
-    //     }, 4000);
-    // }, []);
-    
-
 
     return (
         <div className="container home-page">
@@ -60,14 +46,19 @@ const Home = () => {
                 <img src={LogoTitle} alt="developer"/>
                 <AnimatedLetters letterClass={letterClass}
                 strArray={nameArray}
-                idx={15}/>
+                idx={15}
+                />
                 <br />
                 <AnimatedLetters letterClass={letterClass}
                 strArray={jobArray}
-                idx={22}/>
+                idx={22}
+                />
                 </h1>
                 <h2>Software Developer / Python Expert / Student</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                <Link to="/contact" className="flat-button">
+                CONTACT ME
+                </Link>
+                <Logo />
             </div>
         </div>
     )
