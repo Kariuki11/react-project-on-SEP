2f55a39b1cba09195f3590c021610e672f20dc0d
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-02 16:16:51 +0300

    New changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,3 +1,83 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import LogoS from '../../assets/images/logo-s (1).png';
+import LogoSubtitle from '../../assets/images/logo_sub (1).png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="Main Logo" />
+            <img className='sub-logo' src={LogoSubtitle} alt="Logo Subtitle" />
+        </Link>
+        <nav>
+            <NavLink exact={true} activeClassName="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact={true} activeClassName="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact={true} activeClassName="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+    </div>
+);
+
+export default Sidebar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import { Link, NavLink } from 'react-router-dom'
 // import './index.scss'
 // import LogoS from '../../assets/images/logo-s (1).png'
