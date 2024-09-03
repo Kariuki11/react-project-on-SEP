93715f7b763adab7b3cd84ba93942254404c9021
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-03 14:07:48 +0300

    New changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,17 @@
+import Loader from 'react-loaders';
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
 
 const Contact = () => {
     return (<>
-    <div className='container contact-page'></div>
+    <div className='container contact-page'>
+        <div className='text-zone'>
+            <h1>
+                <AnimatedLetters />
+            </h1>
+        </div>
+    </div>
+    <Loader type="pacman" />
     </>)
 }
 
