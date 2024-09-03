ec4b080493fca9316f00b2d59dce57b74c9e1503
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-03 13:20:21 +0300

    New changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Routes, Route }from 'react-router-dom';
-//import About from './components/About'
+import About from './components/About'
 import Layout from './components/Layout';
 import Home from './components/Home';
 
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
