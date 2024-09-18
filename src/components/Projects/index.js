748a9dd944abee987cbca8be25f0848c2a6078a6
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-18 14:59:51 +0300

    Changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,7 +9,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import  {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const Projects = () => {
